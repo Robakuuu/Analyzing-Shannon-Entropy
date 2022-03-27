8179173856a5640169e3f219944b68a84a717450
Robakuuu <daniel.robaszkiewicz@wp.pl>
2022-03-27 22:06:04 +0200

    fasta reader tested

---
+++ Shannon/Shannon/Entropy/FastaReader.cs
@@ -0,0 +1,27 @@
+﻿using Xyaneon.Bioinformatics.FASTA;
+using Xyaneon.Bioinformatics.FASTA.IO;
+using Standard.Data.StringMetrics;
+
+namespace Shannon.Entropy
+{
+    public class FastaReader
+    {
+        public FastaReader()
+        {
+            Sequences = new List<Sequence>();
+        }
+
+        public IEnumerable<Sequence> Sequences { get; set; }
+
+        public void ReadFile(string path)
+        {
+            Sequences = SequenceFileReader.ReadMultipleFromFile(path);
+        }
+
+        public void Test(string firstseq, string secondseq)
+        {
+            NeedlemanWunch nw= new NeedlemanWunch();
+            nw.GetSimilarityExplained(firstseq, secondseq);
+        }
+    }
+}
