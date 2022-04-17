7f9b43b1ca39350b762d0cdb4c088b7424c8dd9e
Agata Kopyt <agatakopyt@MacBook-Air-Agata.local>
2022-04-17 11:06:20 +0200

    Generating plot in  python

---
+++ Shannon/PythonScripts/Charts/SeqEntropy.py
@@ -0,0 +1,57 @@
+from curses.ascii import isalnum
+from pickle import TRUE
+import matplotlib.pyplot as plt
+import sys
+from os.path import exists
+
+
+class SeqEntropy:
+    def __init__(self):
+        self.args= sys.argv
+        self.pathToFile= self.GetPathToFileFromArgs(self.args)
+        self.Start(self.pathToFile)
+
+    def Start(self,pathToFile):
+        if not self.CheckFileExisitng(pathToFile):
+            exit(0)
+        datas=self.LoadFileData(pathToFile)
+        datas=self.PrepareData(datas)
+        title=self.GetFileName(pathToFile)
+        self.GeneratePlot(datas,title)
+        print(datas)
+        
+    def GetFileName(self,pathToFile:str):
+        index= pathToFile.rfind("/")
+        return pathToFile[index+1:]
+    def GeneratePlot(self,datas,title):
+        xpoints = datas
+        ypoints=[]
+        for index in range(len(xpoints)):
+            ypoints.append(index+1)
+        plt.plot(ypoints,xpoints, linestyle = 'dotted')
+        plt.title("Plot generated from: "+title)
+        plt.show()
+    def PrepareData(self,data):
+        returnData =[]
+        for item in data:
+            if '\n' in item:
+                item=item[:-1]
+            returnData.append(float(item))
+        return returnData
+            
+    def LoadFileData(self,filePath):
+        file = open(filePath,"r")
+        data = file.readlines()
+        file.close()
+        return data
+    def GetPathToFileFromArgs(self,args):
+        for arg in args:
+            if ".py" not in arg:
+                return  str(arg)
+        return ""
+    def CheckFileExisitng(self,filePath):
+        if exists(filePath):
+            return  True
+        return False
+
+app = SeqEntropy()
