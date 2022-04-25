c3511986a9a5de1abb482693bbd6799eca793a29
Robakuuu <daniel.robaszkiewicz@wp.pl>
2022-04-26 01:47:06 +0200

    EntropyCal

---
+++ Shannon/Shannon/EntropyCal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShannonEntropyCal
+{
+    public class EntropyCal
+    {
+        /// <summary>
+        /// Gets the entropy value of a message.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public double EntropyValue(string message)
+        {
+            Dictionary<char, int> K = message.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+            double EntropyValue = 0;
+            foreach (var character in K)
+            {
+                double PR = character.Value / (double)message.Length;
+                EntropyValue -= PR * Math.Log(PR, 2);
+            }
+            return EntropyValue;
+        }
+
+        /// <summary>
+        /// Calculates the Entropy Bit value of a message.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public double EntropyBits(string message)
+        {
+            Dictionary<char, int> K = message.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+            double EntropyValue = 0;
+            foreach (var character in K)
+            {
+                double PR = character.Value / (double)message.Length;
+                EntropyValue -= PR * Math.Log(PR, 2);
+            }
+            return Math.Ceiling(EntropyValue) * message.Length;
+        }
+    }
+}
