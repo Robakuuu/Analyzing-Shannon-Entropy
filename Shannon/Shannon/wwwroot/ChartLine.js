abd2118a7bc428120835756c469c5976a16d7dbb
Robakuuu <daniel.robaszkiewicz@wp.pl>
2022-05-09 22:20:05 +0200

    new chart

---
+++ Shannon/Shannon/wwwroot/ChartLine.js
@@ -0,0 +1,10 @@
+﻿
+function test(chart1, chart2) {
+    LINECHART = document.getElementById('LineChart');
+    var layout = {
+        title: 'Line and Scatter Plot'
+    };
+    var data = [chart1,chart2];
+    Plotly.newPlot(LINECHART, data, layout);
+};
+
