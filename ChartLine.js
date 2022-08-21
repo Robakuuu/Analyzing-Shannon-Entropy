9c99a9732653fc2d25fea3e2e5bc96b2b0ccb131
Robakuuu <68747204+Robakuuu@users.noreply.github.com>
2022-08-21 13:14:50 +0000

    Deploying to gh-pages from  @ 2621eef7cedb48af4c7394fb6181796b9419975d 🚀

--- ChartLine.js
+++ ChartLine.js
@@ -1,10 +1,28 @@
 ﻿
-function test(datas) {
+function test(datas,xaxisname,yaxisname) {
     LINECHART = document.getElementById('LineChart');
     var example = JSON.parse(datas);
     var layout = {
         showlegend: true,
-        legend: { "orientation": "h" }
+        legend: { "orientation": "h" },
+        xaxis: {
+            title: {
+                text: xaxisname,
+                font: {
+                    size: 18,
+                    color: '#7f7f7f'
+                }
+            },
+        },
+        yaxis: {
+            title: {
+                text: yaxisname,
+                font: { 
+                    size: 18,
+                    color: '#7f7f7f'
+                }
+            }
+        }
     };
     Plotly.newPlot(LINECHART, example, layout);
 };
