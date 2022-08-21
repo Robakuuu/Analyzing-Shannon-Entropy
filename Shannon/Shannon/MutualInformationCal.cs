b805ed370e531510303420f3801cba663d9c0f0f
robakuuu <daniel.robaszkiewicz@wp.pl>
2022-08-21 13:00:27 +0200

    average deleted, conditional added

--- Shannon/Shannon/MutualInformationCal.cs
+++ Shannon/Shannon/MutualInformationCal.cs
@@ -17,19 +17,13 @@ public class MutualInformationCal
         }
         return 0.0;
     }
-    public double CalculateAverageMI(string seq1, string seq2) //most right way
+    public double CalculateConditionalEntropy(string seq1, string seq2) //most right way
     {
         if (seq1 != seq2)
         {
             EntropyCal entorpy = new EntropyCal();
-            var HX = entorpy.EntropyValue(seq1);
-            var HY = entorpy.EntropyValue(seq2);
             var HXIY = entorpy.ConditionalEntropyValue(seq1, seq2);
-            var HYIX = entorpy.ConditionalEntropyValue(seq2, seq1);
-            var tmp1 = HX - HXIY;
-            var tmp2 = HY - HYIX;
-            var res = (tmp1 + tmp2) /2.0;
-            return res;
+            return HXIY;
         }
         return 0.0;
     }
